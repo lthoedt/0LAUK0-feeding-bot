6d03273ee801e91e23c8e34961a57f1241338c3f
m <m>
2025-03-18 18:59:55 +0100

    Added sleep in the main loop to prevent busy waiting

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import logging
+
 from MainSystem import MainSystem
+from time import sleep
 
 class Robot:
     def __init__(self) -> None:
@@ -6,8 +9,16 @@ class Robot:
 
     def loop(self):
         self.system.loop()
+        # This sleep intentionally slows down the system.
+        # We currently (and might never) have code in place to do lazy waiting,
+        # so we need to intentionally sleep our infinite loops in order to not
+        # use 100% on busy waits and heat our RPi unnecessarily.
+        sleep(0.05)  # 50ms <==> ~20 Hz
 
 if __name__ == '__main__':
+    # DEBUG output and above gets written to stdout
+    logging.basicConfig(level=logging.DEBUG)
+
     robot = Robot()
     while True:
         robot.loop()
