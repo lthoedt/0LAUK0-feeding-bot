0eb37394d093ef694149a534b85a6de0145da9a3
Leander ten Hoedt <lthoedt@gmail.com>
2025-03-21 12:31:09 +0100

    ~ small useless code removed

--- components/Door.py
+++ components/Door.py
@@ -10,23 +10,18 @@ class Door:
 
     def __init__(self) -> None:
         self.servo = ServoWrapper(18, 0.5)
-        return
 
     def open(self) -> None:
         self.servo.goToAngle(openedAngle)
-        return
 
     def isOpen(self) -> bool:
         return currentAngle == openedAngle
 
     def close(self) -> None:
         self.servo.goToAngle(closedAngle)
-        return
 
     def isClosed(self) -> bool:
         return currentAngle == closedAngle
-        # TODO: Implement
-        return True
 
 
 class FakeDoor(Door):
