aad99dce3c2e6082108aebf46ec94a029018368f
Leander ten Hoedt <lthoedt@gmail.com>
2025-03-21 12:27:52 +0100

    ~ new servo implementation

--- components/ServoWrapper.py
+++ components/ServoWrapper.py
@@ -1,6 +1,7 @@
 import time
-from servo import Servo
 from Camera import Camera
+import RPi.GPIO as GPIO
+import pigpio
 
 class ServoWrapper:
     currentAngle = 0
@@ -8,25 +9,33 @@ class ServoWrapper:
 
     def __init__(self, pin, speed) -> None:
         self.pin = pin
-        self.speed = speed
-        self.servo = Servo(pin_id=pin)
+        self.speed = mapSpeedToMillis(speed)
+
+        self.pwm = pigpio.pi()
+        self.pwm.set_mode(self.pin, pigpio.OUTPUT)
+        self.pwm.set_PWM_frequency(self.pin, 50)
 
     # 1 is 0 delay, 0 is 40 milliseconds
-    def mapSpeedToMillis(self) -> Number:
-        return (1 - self.speed) * 40
+    def mapSpeedToMillis(self, speed) -> Number:
+        return (1 - speed) * 40
+
+    def mapAngleToDutyCycle(self, angle) -> Number:
+        return (angle / 180) * 2000 + 500
 
     def goToAngle(self, angle):
         currentMillis = time.time_ns() // 1_000_000
 
-        if ( currentMillis >= (previousMillis + mapSpeedToMillis()) ):
+        if ( currentMillis >= (previousMillis + self.speed) ):
             if (angle != self.currentAngle):
                 if ( angle > servoCurrentAngle ):
                     servoCurrentAngle += 1
-                else :
+                else:
                     servoCurrentAngle -= 1
-        
+            else:
+                self.pwm.set_PWM_dutycycle( self.pin, 0 )
+                self.pwm.set_PWM_frequency( self.pin, 0 )
             previousMillis = currentMillis
         
-        servo.write( servoCurrentAngle )
+        self.pwm.set_servo_pulsewidth( self.pin, mapAngleToDutyCycle(servoCurrentAngle) )
 
         return servoCurrentAngle == angle
